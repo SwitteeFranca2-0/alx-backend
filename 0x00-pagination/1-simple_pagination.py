9d3c7a905249ae333ca3871c82861eb3986b1d16
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-07-24 23:36:22 +0100

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -29,6 +29,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get page function"""
         assert type(page) == int, AssertionError
         assert type(page_size) == int, AssertionError
         assert page > 0, AssertionError
