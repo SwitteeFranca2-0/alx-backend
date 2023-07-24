2221561181e45bea240342417b3548a74e2a0904
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-07-24 23:33:52 +0100

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,6 +3,6 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple:
     """Function returning a tuple"""
     return (page_size*(page-1), page_size*page)
