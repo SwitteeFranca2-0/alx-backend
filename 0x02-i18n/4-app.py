c6d381f6041de65fb9d7f238f21705a18c253998
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-08-02 17:18:02 +0100

    updates

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -13,18 +13,29 @@ app = Flask(__name__)
 class Config:
     """configuration class"""
     LANGUAGES: List = ["en", "fr"]
-    default_locale: str = "en"
-    default_timezone: str = "UTC"
+    BABEL_DEFAULT_LOCALE: str = "en"
+    BABEL_DEFAULT_TIMEZONE: str = "UTC"
 
 
 app.config.from_object(Config)
-babel = Babel()
-babel.init_app(app)
+babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
     """get location function"""
+    queries = request.query_string.decode('utf-8').split("&")
+    query_dict = {}
+    for query in queries:
+        if "=" in query:
+            arg = query
+        else:
+            arg = "0=" + query
+        args = arg.split("=")
+        query_dict[args[0]] = args[1]
+    if "locale" in query_dict:
+        if query_dict["locale"] in app.config['LANGUAGES']:    
+            return query_dict["locale"]
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
